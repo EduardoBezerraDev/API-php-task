29f4075633a1764876e6822b1d55b0b4b388ec66
Eduardo Bezerra <92584609+EduardoBezerraDev@users.noreply.github.com>
2024-03-28 17:43:00 -0300

    :sparkles: feat: adicionado novo endpoint para deletar

--- src/model/Task.php
+++ src/model/Task.php
@@ -1,13 +1,16 @@
 <?php
+namespace Model;
 
-class Task {
+class Task
+{
     private int $id;
     private string $name;
     private string $startDate;
     private string $endDate;
     private string $status;
 
-    public function __construct(string $name, string $startDate, string $endDate, string $status, int $id = 0) {
+    public function __construct(string $name, string $startDate, string $endDate, string $status, int $id = 0)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->startDate = $startDate;
@@ -15,23 +18,28 @@ class Task {
         $this->status = $status;
     }
 
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getStartDate(): string {
+    public function getStartDate(): string
+    {
         return $this->startDate;
     }
 
-    public function getEndDate(): string {
+    public function getEndDate(): string
+    {
         return $this->endDate;
     }
 
-    public function getStatus(): string {
+    public function getStatus(): string
+    {
         return $this->status;
     }
 }
