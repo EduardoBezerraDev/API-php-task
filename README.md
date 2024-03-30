4de342d4256bb6ee2c9d221e35fe9eda259286bc
EduardoBezerraDev <profissional.edu.teixeira@gmail.com>
2024-03-30 12:23:32 -0300

    :sparkles: feat: Adicionado documentação e banco de dados

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto: Evento
+
+O projeto tem como objetivo criar um todo, no qual é possível cadastrar uma nova task , assim como também visualizar, editar e deletar.
+
+
+### Requisitos
+
+Antes de executar o projeto, certifique-se de ter os seguintes requisitos instalados em seu ambiente de desenvolvimento:
+
+* PHP 8 +: Certifique-se de ter o Node.js instalado em sua máquina. O projeto requer o Node.js para executar o ambiente de desenvolvimento e gerenciar as dependências.
+* MySql/XAMPP: Você precisa ter o Mysql instalado, ou o XAMPP.
+* Gerenciador de Pacotes: O projeto utiliza o composer como gerenciador de pacotes. 
+* Editor de Código: Recomendo o uso de um editor de código como Visual Studio Code ou PhpStorm.
+
+### Instalação
+vendor/bin/phpunit src/tests/TaskAPITest.php
+Após garantir que os requisitos acima estão atendidos, siga as etapas abaixo para executar o projeto:
+
+```
+$ Clone esse repositório: git clone https://github.com/EduardoBezerraDev/tasklist-back-php
+$ Restaure o banco de dados mytasks incluído no repositório
+$ Entre na pasta (tasklist-back-php) e realize o comando "composer install"
+$ Execute o projeto com o comando "php -S localhost:8000"
+```
+
+### Back-End
+
+* O back-end para conseguir rodar esse projeto se encontra nesse repositório: https://github.com/EduardoBezerraDev/tasklist-back-php.git
---
+++ mytasks.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 30/03/2024 às 16:20
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `mytasks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `name`, `startDate`, `endDate`, `status`) VALUES
+(2, 'Comprar pão de coco teste', '2024-03-01', '2024-03-02', 'Concluído');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
